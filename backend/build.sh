c758b7a6e06920a611ee4658a6a4190205667e58
ScrapeFlow159 <arobaarif271@gmail.com>
2026-06-19 17:13:05 +0500

    final fixed

--- backend/build.sh
+++ backend/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install --no-cache-dir -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port $PORT
+python -m pip install --no-cache-dir -r requirements.txt
+python -m uvicorn main:app --host 0.0.0.0 --port $PORT
